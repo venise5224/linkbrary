edaea467eb7b4b5c787ba20a7e5cff7733a06234
mungyun <mungyun1234@naver.com>
2024-11-06 19:50:54 +0900

    Fix: 버튼 코드 리뷰 반영 수정

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,11 +1,11 @@
-import React, { ReactNode } from "react";
+import React, { ReactNode, ButtonHTMLAttributes } from "react";
 
-interface ButtonProps {
+interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   children: ReactNode;
   width?: string;
   height?: string;
   radius?: string;
-  type?: "positive" | "negative";
+  color?: "positive" | "negative";
   size?: string;
 }
 
@@ -14,11 +14,12 @@ const Button = ({
   width = "auto",
   height = "53px",
   radius = "8px",
-  type = "positive",
+  color = "positive",
   size = "18px",
+  ...props
 }: ButtonProps) => {
   const backgroundStyle =
-    type === "positive"
+    color === "positive"
       ? "linear-gradient(90.99deg, #6D6AFE 0.12%, #6AE3FE 101.84%)"
       : "#FF5B56";
 
@@ -32,6 +33,7 @@ const Button = ({
         fontSize: size,
       }}
       className="flex justify-center items-center text-white font-[600] whitespace-nowrap"
+      {...props}
     >
       {children}
     </button>
