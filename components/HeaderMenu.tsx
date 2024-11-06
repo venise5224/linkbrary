152925653aeae222ace74c90077778b737826160
JSM <venise5224@gmail.com>
2024-11-06 20:49:54 +0900

    Refactor: onClick 코드 수정

--- components/HeaderMenu.tsx
+++ components/HeaderMenu.tsx
@@ -1,26 +1,42 @@
 import Image from "next/image";
-import Profile from "@/public/icons/profile.png"
-import Star from "@/public/icons/star.png"
+import Profile from "@/public/icons/profile.png";
+import Star from "@/public/icons/star.png";
 import Link from "next/link";
 import Button from "./Button";
 import { useRouter } from "next/router";
 
 const HeaderMenu = () => {
-    const isLoggedIn = false;
-    const router = useRouter()
+  const isLoggedIn = false;
+  const router = useRouter();
 
-    const handleClick = () => {
-        router.push('/login')
-    }
-
-    return (
+  return (
     <>
       {!isLoggedIn ? (
-          <Button onClick={handleClick} width="w-[80px] md:w-[128px]" height="h-[37px] md:h-[53px]" size="text-[14px] md:text-[18px]" type="button">로그인</Button>
+        <Button
+          onClick={() => {
+            router.push("/login");
+          }}
+          width="w-[80px] md:w-[128px]"
+          height="h-[37px] md:h-[53px]"
+          size="text-[14px] md:text-[18px]"
+          type="button"
+        >
+          로그인
+        </Button>
       ) : (
         <div className="flex items-center gap-[24px]">
-          <Link href={'/favorite'} className="flex items-center gap-[6px] bg-gray200 border border-purple100 rounded-[4px] py-[10px] px-[12px] text-[12px] leading-[14.32px] md:text-[14px] md:leading-[16.71px] font-normal">
-            <Image src={Star} width={14} height={17} alt="" className="align-top"/>즐겨찾기
+          <Link
+            href={"/favorite"}
+            className="flex items-center gap-[6px] bg-gray200 border border-purple100 rounded-[4px] py-[10px] px-[12px] text-[12px] leading-[14.32px] md:text-[14px] md:leading-[16.71px] font-normal"
+          >
+            <Image
+              src={Star}
+              width={14}
+              height={17}
+              alt=""
+              className="align-top"
+            />
+            즐겨찾기
           </Link>
           <div className="flex items-center gap-[6px] text-[14px] leading-[16.71px] font-normal">
             <Image src={Profile} width={28} height={28} alt="프로필" />
@@ -31,7 +47,6 @@ const HeaderMenu = () => {
       )}
     </>
   );
-}
+};
 
 export default HeaderMenu;
-
