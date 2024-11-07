48a5c5588647551a320f1cdb88e12504d9c7e629
mungyun <mungyun1234@naver.com>
2024-11-07 20:19:57 +0900

    Chore: AuthInput 디자인 수정

--- components/Auth/AuthInput.tsx
+++ components/Auth/AuthInput.tsx
@@ -29,32 +29,34 @@ const AuthInput = ({
       <label htmlFor={name} className="text-[14px] block mb-[12px]">
         {text}
       </label>
-      <div className="relative">
-        <input
-          {...props}
-          id={name}
-          name={name}
-          value={value}
-          onChange={onChange}
-          type={isPasswordVisible && type === "password" ? "text" : type}
-          className={`w-full h-[60px] rounded-lg border px-[15px] py-[18px] pr-[40px] outline-purple100 ${
-            error ? "border-red-500" : "border-gray300"
-          }`}
-        />
-        {type === "password" && (
-          <Image
-            src={
-              isPasswordVisible
-                ? "/icons/eyes_open.svg"
-                : "/icons/eyes_close.svg"
-            }
-            width={16}
-            height={16}
-            alt={isPasswordVisible ? "비밀번호 숨기기" : "비밀번호 보기"}
-            onClick={toggleClick}
-            className="absolute top-1/2 right-4 transform -translate-y-1/2 cursor-pointer"
+      <div>
+        <div className="relative">
+          <input
+            {...props}
+            id={name}
+            name={name}
+            value={value}
+            onChange={onChange}
+            type={isPasswordVisible && type === "password" ? "text" : type}
+            className={`w-full h-[60px] rounded-lg border px-[15px] py-[18px] pr-[40px] outline-purple100 ${
+              error ? "border-red-500" : "border-gray300"
+            }`}
           />
-        )}
+          {type === "password" && (
+            <Image
+              src={
+                isPasswordVisible
+                  ? "/icons/eyes_open.svg"
+                  : "/icons/eyes_close.svg"
+              }
+              width={16}
+              height={16}
+              alt={isPasswordVisible ? "비밀번호 숨기기" : "비밀번호 보기"}
+              onClick={toggleClick}
+              className="absolute top-1/2 right-4 transform -translate-y-1/2 cursor-pointer"
+            />
+          )}
+        </div>
         {error && <div className="text-red-500 text-sm mt-1">{error}</div>}
       </div>
     </div>
