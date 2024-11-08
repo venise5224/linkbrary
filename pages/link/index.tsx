6f172403ac5c3cefa0cd33b70a05723f0ed459ba
junjeeong <wn8624@naver.com>
2024-11-08 17:42:58 +0900

    Feat: Link 페이지 기본 ui 구현, 기능 추가해야 함

---
+++ pages/link/index.tsx
@@ -0,0 +1,67 @@
+import { SearchInput } from "../../components/Search/SearchInput";
+import AddLinkInput from "../../components/Link/AddLinkInput";
+import Container from "@/components/Layout/Container";
+import FolderTag from "../../components/FolderTag";
+import CardsLayout from "@/components/Layout/CardsLayout";
+import CardItem from "@/components/CardItem";
+import Image from "next/image";
+
+const LinkPage = () => {
+  return (
+    <>
+      <div className="bg-gray100 w-full h-[219px] flex justify-center items-center">
+        <AddLinkInput folderId={697} />
+      </div>
+      <main className="mt-[40px]">
+        <Container>
+          <SearchInput />
+          <div className="flex justify-between mt-[40px]">
+            <FolderTag list={list} />
+            <button className="w-[79px] h-[19px] text-purple100">
+              폴더 추가 +
+            </button>
+          </div>
+          <div className="flex justify-between items-center mt-[24px]">
+            <h1 className="text-2xl ">유용한 글</h1>
+            <div className="w-[192px] h-[18px] flex justify-between gap-[12px] text-gray400">
+              <button className="flex items-center gap-[4px]">
+                <Image
+                  width={18}
+                  height={18}
+                  src="/icons/share.svg"
+                  alt="share button"
+                />
+                <span>공유</span>
+              </button>
+              <button className="flex items-center gap-[4px]">
+                <Image
+                  width={18}
+                  height={18}
+                  src="/icons/pen.svg"
+                  alt="share button"
+                />
+                <span>수정</span>
+              </button>
+              <button className="flex items-center gap-[4px]">
+                <Image
+                  width={18}
+                  height={18}
+                  src="/icons/delete.svg"
+                  alt="delete button"
+                />
+                <span>삭제</span>
+              </button>
+            </div>
+          </div>
+          <CardsLayout>
+            {list.map((link) => (
+              <CardItem key={link.id} info={link} />
+            ))}
+          </CardsLayout>
+        </Container>
+      </main>
+    </>
+  );
+};
+
+export default LinkPage;
