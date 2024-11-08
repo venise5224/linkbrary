c36ee7bb2c820215c9603b2ea2c90101d875b0e5
hongggy <hongggy@gmail.com>
2024-11-08 15:13:40 +0900

    Feat: 테스트 페이지 모달 열기

--- pages/test/index.tsx
+++ pages/test/index.tsx
@@ -1,13 +1,16 @@
-import useModalStore from "@/store/useModalStore";
+import AddFolderModal from "@/components/modal/AddFolderModal";
+import { Switch } from "@/components/modal/modalManager/ModalManager";
+import useModal from "@/hooks/useModal";
 
 export default function Test() {
-  const addFolderModal = useModalStore();
+  const { isOpen, onOpen } = useModal("AddFolderModal");
 
   return (
     <div className="m-20">
-      <button type="button" onClick={() => addFolderModal.openModal}>
+      <button type="button" onClick={() => onOpen()}>
         버튼
       </button>
+      {isOpen && <Switch />}
     </div>
   );
 }
