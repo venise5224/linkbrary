115dcae26bbf4f117dd11c4e04ba4146c64ce819
99uminji <rnalswl96@naver.com>
2024-11-08 14:47:49 +0900

    Design: CardsLayout css 수정

--- components/Layout/CardsLayout.tsx
+++ components/Layout/CardsLayout.tsx
@@ -6,7 +6,7 @@ interface CardsLayoutProps {
 
 const CardsLayout = ({ children }: CardsLayoutProps) => {
   return (
-    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 md:gap-6 lg:gap-[10px] w-full">
+    <div className="grid place-items-center grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 md:gap-6 lg:gap-[10px] w-full">
       {children}
     </div>
   );
