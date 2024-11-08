77bb2260bddce64d1f9721f17b12b97892cb4d3e
JSM <venise5224@gmail.com>
2024-11-08 19:28:34 +0900

    Style:lg CSS 추가

--- components/Layout/Header.tsx
+++ components/Layout/Header.tsx
@@ -5,11 +5,11 @@ import HeaderMenu from "../HeaderMenu";
 
 const Header = () => {
   return (
-    <header className="bg-gray100 py-[13px] px-[32px] md:py-[33px] md:px-[200px]">
+    <header className="bg-gray100 py-[13px] px-[32px] md:py-[33px] md:px-[200px] lg:py-[33px] lg:px-[200px]">
       <div className="flex justify-between items-center max-w-[1520px]">
         <Link
           href={"/"}
-          className="w-[88.67px] h-[16px] md:w-[133px] md:h-[24px]"
+          className="w-[88.67px] h-[16px] md:w-[133px] md:h-[24px] lg:w-[133px] lg:h-[24px]"
         >
           <Image src={Logo} width={133} height={24} alt="로고" />
         </Link>
