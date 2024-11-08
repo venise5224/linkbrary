7903325976f850475f71978d682d44c1d30a9544
hongggy <hongggy@gmail.com>
2024-11-08 15:57:57 +0900

    Chore : 폴더리스트 목데이터 추가

--- components/modal/modalManager/ModalManager.tsx
+++ components/modal/modalManager/ModalManager.tsx
@@ -26,7 +26,18 @@ export const Switch = () => {
     case "AddFolderModal":
       return <AddFolderModal folderName={props.folderName || ""} />;
     case "AddModal":
-      return <AddModal list={props.list || []} />;
+      return (
+        <AddModal
+          list={
+            props.list || [
+              { id: 1, title: "코딩팁", totalCount: 7 },
+              { id: 2, title: "채용 사이트", totalCount: 7 },
+              { id: 3, title: "유용한 글", totalCount: 7 },
+              { id: 4, title: "나만의 장소", totalCount: 7 },
+            ]
+          }
+        />
+      );
     case "DeleteFolderModal":
       return <DeleteFolderModal folderName={props.folderName || ""} />;
     case "DeleteLinkModal":
