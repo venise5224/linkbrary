bdd0164e293a4ecf57dcbe0e81de7a39ee4a9f39
junjeeong <wn8624@naver.com>
2024-11-08 13:37:44 +0900

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,4 @@
-import SearchInput from "@/components/Search/SearchInput";
-
 export default function Home() {
   return (
-    <div className="flex justify-center items-center mt-[20px]">
-      <SearchInput />
-    </div>
   );
 }
