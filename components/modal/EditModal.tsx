00756c8ff68320d43d437a782277394c97bced1c
hongggy <hongggy@gmail.com>
2024-11-09 11:18:28 +0900

    Feat: 폴더 수정 모달 작업 중

--- components/modal/EditModal.tsx
+++ components/modal/EditModal.tsx
@@ -1,10 +1,43 @@
+import { ChangeEvent, useState } from "react";
 import ModalContainer from "./modalComponents/ModalContainer";
 import ModalInput from "./modalComponents/ModalInput";
+import useModalStore from "@/store/useModalStore";
+import { putFolder } from "@/lib/api/folder";
 
 const EditModal = ({ folderName }: { folderName: string }) => {
+  const [value, setValue] = useState("");
+
+  const { closeModal } = useModalStore();
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setValue(e.target.value);
+  };
+  const handleSubmit = async () => {
+    const body = {
+      name: value,
+    };
+    // if (value !== "") {
+    //   try {
+    //     const res = await putFolder(folderId, body);
+    //     console.log(res);
+    //   } catch (error) {
+    //     console.log(error);
+    //   }
+    // }
+    closeModal();
+  };
   return (
-    <ModalContainer title="폴더 이름 변경" buttonText="변경하기">
-      <ModalInput placeholder="내용 입력" name={folderName} />
+    <ModalContainer
+      title="폴더 이름 변경"
+      buttonText="변경하기"
+      onClick={handleSubmit}
+    >
+      <ModalInput
+        placeholder="내용 입력"
+        name={folderName}
+        value={value}
+        onChange={handleChange}
+      />
     </ModalContainer>
   );
 };
