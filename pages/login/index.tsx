bbae24f7a3b223a0bdeb8cc1bf451f20210aa2b7
mungyun <mungyun1234@naver.com>
2024-11-09 14:45:13 +0900

    Chore: 로그인 및 회원가입 페이지 배경 수정

--- pages/login/index.tsx
+++ pages/login/index.tsx
@@ -10,52 +10,54 @@ const Login = () => {
     useForm(false);
 
   return (
-    <AuthLayout>
-      <p className="mt-[16px] text-base font-normal">
-        회원이 아니신가요?{" "}
-        <Link
-          href="/signup"
-          className="cursor-pointer text-purple100 underline font-semibold"
+    <div className="bg-gray100 min-h-screen">
+      <AuthLayout>
+        <p className="mt-[16px] text-base font-normal">
+          회원이 아니신가요?{" "}
+          <Link
+            href="/signup"
+            className="cursor-pointer text-purple100 underline font-semibold"
+          >
+            회원 가입하기
+          </Link>
+        </p>
+        <form
+          className="w-full sm:max-w-[325px] md:max-w-[400px] lg:max-w-[400px] mt-[30px]"
+          aria-labelledby="login-form"
+          onSubmit={handleSubmit}
         >
-          회원 가입하기
-        </Link>
-      </p>
-      <form
-        className="w-full sm:max-w-[325px] md:max-w-[400px] lg:max-w-[400px] mt-[30px]"
-        aria-labelledby="login-form"
-        onSubmit={handleSubmit}
-      >
-        <AuthInput
-          text="이메일"
-          type="text"
-          name="email"
-          placeholder="이메일을 입력해주세요."
-          value={values.email}
-          onChange={handleChange}
-          onBlur={handleBlur}
-          error={errors.email}
-        />
-        <AuthInput
-          text="비밀번호"
-          type="password"
-          name="password"
-          placeholder="비밀번호를 입력해주세요."
-          value={values.password}
-          onChange={handleChange}
-          onBlur={handleBlur}
-          error={errors.password}
-        />
-        <SubmitButton
-          type="submit"
-          width="w-full"
-          height="h-[53px]"
-          className="mt-[30px]"
-        >
-          로그인
-        </SubmitButton>
-        <SnsLogin />
-      </form>
-    </AuthLayout>
+          <AuthInput
+            text="이메일"
+            type="text"
+            name="email"
+            placeholder="이메일을 입력해주세요."
+            value={values.email}
+            onChange={handleChange}
+            onBlur={handleBlur}
+            error={errors.email}
+          />
+          <AuthInput
+            text="비밀번호"
+            type="password"
+            name="password"
+            placeholder="비밀번호를 입력해주세요."
+            value={values.password}
+            onChange={handleChange}
+            onBlur={handleBlur}
+            error={errors.password}
+          />
+          <SubmitButton
+            type="submit"
+            width="w-full"
+            height="h-[53px]"
+            className="mt-[30px]"
+          >
+            로그인
+          </SubmitButton>
+          <SnsLogin />
+        </form>
+      </AuthLayout>
+    </div>
   );
 };
 
