4f7619cd345f6c8ed677bf86c9141fdd4d609435
99uminji <rnalswl96@naver.com>
2024-11-10 19:58:12 +0900

    Feat: 링크 수정 모달 생성

---
+++ components/modal/EditLink.tsx
@@ -0,0 +1,51 @@
+import { ChangeEvent, useState } from "react";
+import ModalContainer from "./modalComponents/ModalContainer";
+import ModalInput from "./modalComponents/ModalInput";
+import useModalStore from "@/store/useModalStore";
+import SubmitButton from "../SubMitButton";
+
+const EditLink = ({ folderName }: { folderName: string }) => {
+  const [value, setValue] = useState("");
+
+  const { closeModal } = useModalStore();
+
+  // 링크 정보를 먼저 가져와야함
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setValue(e.target.value);
+  };
+  const handleSubmit = async () => {
+    const body = {
+      name: value,
+    };
+    // if (value !== "") {
+    //   try {
+    //     const res = await putFolder(folderId, body);
+    //     console.log(res);
+    //   } catch (error) {
+    //     console.log(error);
+    //   }
+    // }
+    closeModal();
+  };
+  return (
+    <ModalContainer title="폴더 이름 변경">
+      <ModalInput
+        placeholder="내용 입력"
+        name={folderName}
+        value={value}
+        onChange={handleChange}
+      />
+      <SubmitButton
+        type="button"
+        // onClick={handleSubmit}
+        width="w-full"
+        height="h-[51px] "
+        color="positive"
+      >
+        변경하기
+      </SubmitButton>
+    </ModalContainer>
+  );
+};
+export default EditLink;
--- components/modal/modalManager/ModalManager.tsx
+++ components/modal/modalManager/ModalManager.tsx
@@ -5,6 +5,7 @@ import AddFolderModal from "../AddFolderModal";
 import DeleteLinkModal from "../DeleteLinkModal";
 import EditModal from "../EditModal";
 import SNSModal from "../SNSModal";
+import EditLink from "../EditLink";
 
 export const ModalType = {
   AddFolderModal: "AddFolderModal",
@@ -13,6 +14,7 @@ export const ModalType = {
   DeleteLinkModal: "DeleteLinkModal",
   EditModal: "EditModal",
   SNSModal: "SNSModal",
+  EditLink: "EditLink",
 } as const;
 
 export type ModalKeysType = keyof typeof ModalType;
@@ -52,5 +54,7 @@ export const Modal = () => {
       return <EditModal folderName={props.folderName || "폴더이름"} />;
     case "SNSModal":
       return <SNSModal folderName={props.folderName || "폴더이름"} />;
+    case "EditLink":
+      return <EditLink folderName={props.folderName || "폴더이름"} />;
   }
 };
