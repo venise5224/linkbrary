0924ff1bb9be0a5eb40f8d1fc4d887c73dd2358d
junjeeong <wn8624@naver.com>
2024-11-10 10:35:51 +0900

    Feat : /link 페이지 SSR로 데이터 받아오는 로직 추가

--- pages/link/index.tsx
+++ pages/link/index.tsx
@@ -1,50 +1,57 @@
 import { GetServerSidePropsContext } from "next";
 import { proxy } from "@/lib/api/axiosInstanceApi";
 import { SearchInput } from "../../components/Search/SearchInput";
+import { LinkData } from "@/types/linkTypes";
+import { FolderData } from "@/types/folderTypes";
 import Container from "@/components/Layout/Container";
 import CardsLayout from "@/components/Layout/CardsLayout";
-import AddLinkInput from "../../components/Link/AddLinkInput";
 import FolderTag from "../../components/FolderTag";
 import LinkCard from "../../components/LinkCard";
-import ActionButtons from "@/components/Link/ActionButtons";
+import AddLinkInput from "@/components/link/AddLinkInput";
+import ActionButtons from "@/components/link/ActionButtons";
 
-const getServerSideProps = async (context: GetServerSidePropsContext) => {
+interface LinkPageProps {
+  links: LinkData[];
+  folders: FolderData[];
+}
+export const getServerSideProps = async (
+  context: GetServerSidePropsContext
+) => {
   const { req } = context;
-  const { cookie } = req.headers;
 
   const fetchData = async (endpoint: string) => {
-    return await proxy.get(endpoint, {
+    const response = await proxy.get(endpoint, {
       headers: {
-        Cookie: cookie,
+        Cookie: req.headers.cookie,
       },
     });
+    return response.data;
   };
 
-  const links = await fetchData("/api/links");
-  const folders = await fetchData("/api/folders");
+  const [links, folders] = await Promise.all([
+    fetchData("/api/links"),
+    fetchData("/api/folders"),
+  ]);
 
   return {
     props: {
-      links: links.data,
-      folders: folders.data,
+      links: links.list || [], // 기본값 처리
+      folders: folders || [], // 기본값 처리
     },
   };
 };
 
-const LinkPage = ({ links, folders }) => {
-  console.log(links);
-  console.log(folders);
-
+const LinkPage = ({ links, folders }: LinkPageProps) => {
   return (
     <>
       <div className="bg-gray100 w-full h-[219px] flex justify-center items-center text-sm">
-        {/* <AddLinkInput folders={folders} /> */}
+        <AddLinkInput folders={folders} />
       </div>
       <main className="mt-[40px]">
         <Container>
           <SearchInput />
           <div className="flex justify-between mt-[40px]">
-            {/* <FolderTag list={list} /> */}
+            <FolderTag list={folders} />
             <button className="w-[79px] h-[19px] text-purple100">
               폴더 추가 +
             </button>
@@ -54,9 +61,9 @@ const LinkPage = ({ links, folders }) => {
             <ActionButtons />
           </div>
           <CardsLayout>
-            {/* {links.map((link) => (
+            {links.map((link) => (
               <LinkCard key={link.id} info={link} />
-            ))} */}
+            ))}
           </CardsLayout>
         </Container>
       </main>
