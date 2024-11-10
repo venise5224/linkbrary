98d1f38b6cbe5b056019a359818afbfb708c27fc
junjeeong <wn8624@naver.com>
2024-11-10 11:30:22 +0900

    Fix : 받는 prop 수정, handleSubmit 로직 수정

--- components/Link/AddLinkInput.tsx
+++ components/Link/AddLinkInput.tsx
@@ -1,30 +1,33 @@
 import { ChangeEvent, FormEvent, useState } from "react";
-import { postLink } from "@/lib/api/link";
+import { FolderData } from "@/types/folderTypes";
 import Image from "next/image";
 import SubmitButton from "../SubMitButton";
 
-const AddLinkInput = (folderId: number) => {
-  const [value, setValue] = useState("");
+interface AddLinkInputProps {
+  folderList: FolderData[];
+}
+
+const AddLinkInput = ({ folderList }: AddLinkInputProps) => {
+  const [link, setLink] = useState("");
 
   const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
-    setValue(e.target.value);
+    setLink(e.target.value);
   };
 
   const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    await postLink({ url: value, folderId });
-    // postLink 하고 추가된 link가 보이도록 하는 로직 구현해야 함.
+    // Addmodal 띄우면서 link, folders 전달해주어야 함.
   };
 
   return (
     <form
       onSubmit={handleSubmit}
-      className="flex gap-[12px] items-center w-[800px] h-[69px] py-[16px] px-[20px] border border-blue-500 rounded-[10px] md:w-[704px] sm:w-[325px] sm:h-[53px] transition-all"
+      className="flex bg-white gap-[12px] items-center w-[800px] h-[69px] py-[16px] px-[20px] border border-blue-500 rounded-[10px] md:w-[704px] sm:w-[325px] sm:h-[53px] transition-all"
     >
       <Image src="/icons/link.svg" width={20} height={20} alt="link icon" />
       <input
         onChange={handleChange}
-        value={value}
+        value={link}
         placeholder="링크를 추가해 보세요."
         className="flex-grow"
       />
