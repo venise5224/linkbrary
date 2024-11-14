ae9405e76562a2aa812d4552a9b45139fb82b58e
JSM <venise5224@gmail.com>
2024-11-15 00:58:56 +0900

    fix: 수정

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,6 +5,10 @@ export default function Document() {
     <Html lang="ko">
       <Head />
       <body className="antialiased">
+        <script
+          defer
+          src="https://developers.kakao.com/sdk/js/kakao.min.js"
+        ></script>
         <Main />
         <NextScript />
       </body>
