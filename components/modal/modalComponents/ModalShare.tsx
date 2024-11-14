518568fb1d5c00d49271cc1a5abe2d0d2b3cb4db
JSM <venise5224@gmail.com>
2024-11-15 02:05:35 +0900

    feat:폴더 공유 기능 구현

--- components/modal/modalComponents/ModalShare.tsx
+++ components/modal/modalComponents/ModalShare.tsx
@@ -1,19 +1,31 @@
+import {
+  handleCopyUrl,
+  handleShareFacebook,
+  handleShareKakao,
+} from "@/lib/utils";
 import ModalShareItem from "./ModalShareItem";
+import { useEffect } from "react";
 
 const ModalShare = () => {
+  useEffect(() => {
+    if (!window.Kakao.isInitialized()) {
+      window.Kakao.init(process.env.NEXT_PUBLIC_KAKAO_API_KEY);
+    }
+  }, []);
+
   return (
     <div className="flex gap-8">
-      <div>
+      <div onClick={handleShareKakao}>
         <ModalShareItem src="/icons/Kakao.svg" text="카카오톡" bg="#FEE500" />
       </div>
-      <div>
+      <div onClick={handleShareFacebook}>
         <ModalShareItem
           src="/icons/Facebook.svg"
           text="페이스북"
           bg="#1877F2"
         />
       </div>
-      <div>
+      <div onClick={handleCopyUrl}>
         <ModalShareItem
           src="/icons/link.svg"
           text="링크 복사"
