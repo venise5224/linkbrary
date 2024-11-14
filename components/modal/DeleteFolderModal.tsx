80b34a624eb5cf521dce0f79c489dcf6ad35d179
hongggy <hongggy@gmail.com>
2024-11-14 21:08:54 +0900

    Feat: 링크 개수 확인 로직 작업 중

--- components/modal/DeleteFolderModal.tsx
+++ components/modal/DeleteFolderModal.tsx
@@ -4,6 +4,8 @@ import ModalContainer from "./modalComponents/ModalContainer";
 import { deleteFolder } from "@/lib/api/folder";
 import toast from "react-hot-toast";
 import toastMessages from "@/lib/toastMessage";
+import { getLink } from "@/lib/api/link";
+import { useState } from "react";
 
 const DeleteFolderModal = ({
   folderName,
@@ -13,13 +15,29 @@ const DeleteFolderModal = ({
   folderId: number;
 }) => {
   const { closeModal } = useModalStore();
+  const [linkCount, setLinkCount] = useState(0);
+
+  // 폴더 내 링크 개수 확인
+  const getLinkCountByFolder = async () => {
+    let query;
+    try {
+      const res = await getLink(query, folderId);
+      const data = res.data;
+      console.log(res);
+      console.log(data);
+      // setLinkCount()
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
   const handleSubmit = async () => {
     // 폴더 내에 링크 개수 0 일때만 삭제 가능 -> 링크 1개 이상이면 error toast 띄우겠습니다.
+    getLinkCountByFolder();
     try {
       await deleteFolder(folderId);
       toast.success(toastMessages.success.deleteFolder);
     } catch (error) {
-      console.log(error, "폴더 삭제 에러");
       toast.error(toastMessages.error.deleteFolder);
     } finally {
       closeModal();
