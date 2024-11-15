a5553112685e8da15c784d67b952ba20466ec5ab
mungyun <mungyun1234@naver.com>
2024-11-15 17:51:56 +0900

    Refactor: axiosInstance에서 불필요한 속성 삭제

--- lib/api/axiosInstanceApi.ts
+++ lib/api/axiosInstanceApi.ts
@@ -2,15 +2,11 @@ import axios from "axios";
 
 const axiosInstance = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_URL || "http://localhost:3000",
-  timeout: 10000,
-  withCredentials: true,
 });
 
 export const proxy = axios.create({
   // 배포 이후에는 배포된 URL로 변경해야 함.
   baseURL: "http://localhost:3000",
-  timeout: 10000,
-  withCredentials: true,
 });
 
 proxy.interceptors.response.use(
