f6725d02a8d07b3564743044fe86b7d2e0ba45b2
hongggy <hongggy@gmail.com>
2024-11-16 15:42:41 +0900

    Chore: 위치 조정

--- components/toTopBtn/ToTopBtn.tsx
+++ components/toTopBtn/ToTopBtn.tsx
@@ -10,7 +10,7 @@ const ToTopBtn = () => {
       <button
         type="button"
         onClick={handleClickToTop}
-        className="fixed bottom-20 right-10 rounded-full border bg-white opacity-70 border-purple100 size-[40px] flex items-center justify-center"
+        className="fixed bottom-10 right-10 rounded-full border bg-white opacity-70 border-purple100 size-[40px] flex items-center justify-center"
       >
         <FaAngleUp fill="purple100" />
       </button>
