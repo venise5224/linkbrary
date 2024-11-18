949a07b8dde8fcdd390e66a055911c79bdb9fa13
hongggy <hongggy@gmail.com>
2024-11-18 18:49:49 +0900

    Refactor: 검색어 debounce 적용

--- components/Search/SearchInput.tsx
+++ components/Search/SearchInput.tsx
@@ -1,22 +1,38 @@
-import { ChangeEvent, FormEvent, useState } from "react";
+import { ChangeEvent, useEffect, useState } from "react";
 import { useRouter } from "next/router";
 import Image from "next/image";
+import useDebounce from "@/hooks/useDebounce";
 
 export const SearchInput = () => {
   const router = useRouter();
   const [value, setValue] = useState("");
 
+  const debouncedValue = useDebounce(value, 200);
+
   const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
     e.preventDefault();
     setValue(e.target.value);
-    router.push({
-      pathname: router.pathname,
-      query: { ...router.query, search: value },
-    });
+  };
+
+  useEffect(() => {
+    if (debouncedValue) {
+      router.push({
+        pathname: router.pathname,
+        query: { ...router.query, search: debouncedValue },
+      });
+    }
+  }, [debouncedValue]);
+
+  const handleClick = () => {
+    setValue("");
+    router.push("/link");
   };
 
   return (
-    <form className="flex gap-[8px] w-full h-[54px] items-center px-[16px] py-[15px] bg-gray-100 rounded-[10px] md:h-[54px] sm:h-[43px] transition-all">
+    <form
+      onSubmit={(e) => e.preventDefault()}
+      className="flex gap-[8px] w-full h-[54px] items-center px-[16px] py-[15px] bg-gray-100 rounded-[10px] md:h-[54px] sm:h-[43px] transition-all"
+    >
       <Image
         src="/icons/search.svg"
         width={16}
@@ -29,6 +45,15 @@ export const SearchInput = () => {
         placeholder="링크를 검색해 보세요."
         className="flex-grow bg-transparent placeholder:text-gray-500"
       />
+      {value && (
+        <button
+          className="rounded-full bg-white size-6 font-bold text-gray-500 flex items-center justify-center"
+          type="button"
+          onClick={handleClick}
+        >
+          ×
+        </button>
+      )}
     </form>
   );
 };
