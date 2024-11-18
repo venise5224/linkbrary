71e16cc1ef5451dd9b2e4e37cb2b56ecf905c8f5
mungyun <mungyun1234@naver.com>
2024-11-18 20:08:49 +0900

    HotFix: 간편 로그인 기능 오류 수정

--- lib/api/auth.ts
+++ lib/api/auth.ts
@@ -1,4 +1,4 @@
-import axiosInstance, { proxy } from "./axiosInstanceApi";
+import { proxy } from "./axiosInstanceApi";
 
 export interface signInProps {
   email: string;
@@ -6,15 +6,6 @@ export interface signInProps {
 }
 
 interface signUpProps extends signInProps {
-  name: string;
-}
-
-export interface easySignInProps {
-  token: string;
-  redirectUri: string;
-}
-
-interface easySignUpProps extends easySignInProps {
   name: string;
 }
 
@@ -32,32 +23,6 @@ export const postSignUp = async (body: signUpProps) => {
 export const postSignIn = async (body: signInProps) => {
   try {
     const res = await proxy.post("/api/auth/sign-in", body);
-    if (res.status >= 200 && res.status < 300) return res.data;
-  } catch (err) {
-    console.error("에러 메시지: ", err instanceof Error ? err.message : err);
-  }
-};
-
-// 간편 회원가입
-export const postEasySignUp = async (
-  provider: "google" | "kakao",
-  body: easySignUpProps
-) => {
-  try {
-    const res = await axiosInstance.post(`/auth/sign-up/${provider}`, body);
-    if (res.status >= 200 && res.status < 300) return res.data;
-  } catch (err) {
-    console.error("에러 메시지: ", err instanceof Error ? err.message : err);
-  }
-};
-
-// 간편 로그인
-export const postEasySignIn = async (
-  provider: "google" | "kakao",
-  body: easySignInProps
-) => {
-  try {
-    const res = await axiosInstance.post(`/auth/sign-in/${provider}`, body);
     if (res.status >= 200 && res.status < 300) return res.data;
   } catch (err) {
     console.error("에러 메시지: ", err instanceof Error ? err.message : err);
--- store/useAuthStore.tsx
+++ store/useAuthStore.tsx
@@ -1,11 +1,6 @@
 import { create } from "zustand";
 import { User } from "@/types/AuthTypes";
-import {
-  postSignIn,
-  signInProps,
-  easySignInProps,
-  postEasySignIn,
-} from "@/lib/api/auth";
+import { postSignIn, signInProps } from "@/lib/api/auth";
 import { getUserInfo } from "@/lib/api/user";
 import { proxy } from "@/lib/api/axiosInstanceApi";
 import { persist } from "zustand/middleware";
@@ -13,36 +8,35 @@ import { persist } from "zustand/middleware";
 interface AuthStore {
   user: User | null;
   login: (body: signInProps) => Promise<boolean>;
-  SNSLogin: (
-    provider: "google" | "kakao",
-    body: easySignInProps
-  ) => Promise<boolean>;
   logout: () => Promise<void>;
+  fetchUserInfo: () => Promise<boolean>;
 }
-
-const fetchUserInfo = async (set: any) => {
-  try {
-    const userInfo = await getUserInfo();
-    if (userInfo) {
-      set({ user: userInfo });
-      return true;
-    }
-  } catch (error) {
-    console.error("사용자 정보 가져오기 에러", error);
-  }
-  return false;
-};
 
 const useAuthStore = create<AuthStore>()(
   persist(
-    (set) => ({
+    (set, get) => ({
       user: null,
 
+      // 사용자 정보 가져오기 함수
+      fetchUserInfo: async () => {
+        try {
+          const userInfo = await getUserInfo();
+          if (userInfo) {
+            set({ user: userInfo });
+            return true;
+          }
+        } catch (error) {
+          console.error("사용자 정보 가져오기 에러", error);
+        }
+        return false;
+      },
+
+      // 로그인 함수
       login: async (body) => {
         try {
           const response = await postSignIn(body);
           if (response) {
-            return await fetchUserInfo(set);
+            return await get().fetchUserInfo();
           }
         } catch (error) {
           console.error("로그인 중 에러가 발생했습니다", error);
@@ -50,22 +44,12 @@ const useAuthStore = create<AuthStore>()(
         return false;
       },
 
-      SNSLogin: async (provider, body) => {
-        try {
-          const response = await postEasySignIn(provider, body);
-          if (response) {
-            return await fetchUserInfo(set);
-          }
-        } catch (error) {
-          console.error("소셜 로그인 중 에러가 발생했습니다.", error);
-        }
-        return false;
-      },
-
+      // 로그아웃 함수
       logout: async () => {
         try {
           await proxy.post("/api/auth/sign-out");
           localStorage.removeItem("auth-storage");
+          set({ user: null });
         } catch (error) {
           console.error("로그아웃 중 에러가 발생했습니다.", error);
         }
